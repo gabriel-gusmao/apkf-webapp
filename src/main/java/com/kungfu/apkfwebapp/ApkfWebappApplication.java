b9c011fb5722c27da38183959e0ad3d378a965f7
Gabriel Eller Gusmão <136000867+gabriel-gusmao@users.noreply.github.com>
2023-09-21 21:47:57 +0100

    Empty project commit

---
+++ src/main/java/com/kungfu/apkfwebapp/ApkfWebappApplication.java
@@ -0,0 +1,13 @@
+package com.kungfu.apkfwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkfWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApkfWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kungfu/apkfwebapp/ApkfWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kungfu.apkfwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApkfWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
